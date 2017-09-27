1fb4f48e9fb19ae93ebee5f078ce3a43721f0a85
haendelsantos <haendelsantos58@gmail.com>
2017-09-26 21:12:53 -0300

    Update O_Conner-1.1.ino

--- O_Conner-1.1.ino
+++ O_Conner-1.1.ino
@@ -8,16 +8,18 @@ Link: https://www.robocore.net/loja/produtos/arduino-shield-motor-driver-2x2a.ht
 */
 #include<stdlib.h>
 #include<stdio.h>
-
-/* Parte ainda não incluida no robo.
-#include "DHT.h"
-#define DHTPIN A1 
+#include<DHT.h>
+#define DHTPIN A15 
 #define DHTTYPE DHT11 
-
 DHT sensor(DHTPIN,DHTTYPE);//Definindo a biblioteca umidade e temperatura.
-const int faroisPin = 2; //Painel de LED
-const int sensorLuzPin = 4; //SENSOR LDR
-*/
+const int faroisPin1 = 1; //Painel de LED
+const int faroisPin2 = 2; //Painel de LED
+const int sensorLuzPin = 14; //SENSOR LDR
+const int sensorTemp = 13;
+//const int rgbCorVermelho = 11;
+//const int rgbCorVerde = 12;
+//const int rgbCorAzul = 13;
+
 
 //CONEXÕES DOS MOTORES. (1) HORARIO, (2) ANTI HORARIO;
 const int motorEsquerdo1 = 5;
@@ -33,28 +35,30 @@ void setup()
   Serial.begin(9600);
   Serial.println("START!!");
   sensor.begin();
-//  pinMode(faroisPin,OUTPUT);
+  pinMode(faroisPin1,OUTPUT);
+  pinMode(faroisPin2,OUTPUT);
   pinMode(motorEsquerdo1, OUTPUT);
   pinMode(motorEsquerdo2, OUTPUT);
   pinMode(motorDireito1, OUTPUT);
   pinMode(motorDireito2, OUTPUT);
+  pinMode(sensorTemp,INPUT);
+//  pinMode(rgbCorVermelho,OUTPUT);
+//  pinMode(rgbCorVerde,OUTPUT);
+//  pinMode(rgbCorAzul,OUTPUT);
 }
- 
 void loop()
 {
-  /* Parte ainda não incluída no Robo 
   //SENSORES
   int valorLuz  = analogRead(sensorLuzPin);
   float umidade = sensor.readHumidity();
-  float temperatura = sensor.readTemperature();
-
-  if (isnan(umidade)){
-    Serial.println("Nao foi possivel obter umidade.");
-  } else{
-    Serial.print("Hum. = ");
-    Serial.print(umidade);
-    Serial.println(" % "); 
-  } 
+  float temperatura = sensorTemp;
+//  if (isnan(umidade)){
+//    Serial.println("Nao foi possivel obter umidade.");
+//  } else{
+//    Serial.print("Hum. = ");
+//    Serial.print(umidade);
+//    Serial.println(" % "); 
+//  } 
   
   if(isnan(temperatura)){
     Serial.println("Nao foi possivel obter temperatura.");  
@@ -63,17 +67,21 @@ void loop()
     Serial.print(temperatura);
     Serial.print(" C ");
   }
-  if(isnan(valorLuz)){
-    Serial.println("Nao foi possivel obter a luminosidade.");
-    digitalWrite(faroisPin,LOW); //Por padrão, luz apagada;
-  } else{
-     if (valorLuz>= 800){ 
-          digitalWrite(faroisPin,HIGH);
-        } else{ 
-          digitalWrite(faroisPin,LOW);
-        }
-     Serial.println(valorLuz);
-  }*/
+//  if(isnan(valorLuz)){
+//    Serial.println("Nao foi possivel obter a luminosidade.");
+//    analogWrite(faroisPin1,LOW); //Por padrão, luz apagada;
+//    analogWrite(faroisPin2,LOW); //Por padrão, luz apagada;
+//  } else{
+//     if (valorLuz <= 300){ 
+//          analogWrite(faroisPin1,HIGH);
+//          analogWrite(faroisPin2,HIGH);
+//        } else{ 
+//          analogWrite(faroisPin1,LOW);
+//          analogWrite(faroisPin2,LOW);
+//        }
+//     Serial.println(valorLuz);
+// }
+  
   //MOTORES
   if(Serial.available()){
     comandoApp = Serial.read();
@@ -123,7 +131,7 @@ void loop()
             digitalWrite(motorEsquerdo1, HIGH);
             digitalWrite(motorDireito1, HIGH);
             analogWrite(motorEsquerdo2, i);//VELOCIDADE
-            j = (i/2)//METADE DA VELOCIDADE
+            j = (i/2);//METADE DA VELOCIDADE
             analogWrite(motorDireito2, j);//VELOCIDADE
             delay(100);
           break; 
@@ -133,7 +141,7 @@ void loop()
             digitalWrite(motorEsquerdo1, LOW);
             digitalWrite(motorDireito1, LOW);
             analogWrite(motorEsquerdo2, i);//VELOCIDADE
-            j = (i/2)//METADE DA VELOCIDADE
+            j = (i/2);//METADE DA VELOCIDADE
             analogWrite(motorDireito2, j);//VELOCIDADE
             delay(100);
           break;
@@ -161,7 +169,7 @@ void loop()
             digitalWrite(motorEsquerdo1, HIGH);
             digitalWrite(motorDireito1, HIGH);
             analogWrite(motorDireito2, i);//VELOCIDADE
-            j = (i/2)//METADE DA VELOCIDADE
+            j = (i/2);//METADE DA VELOCIDADE
             analogWrite(motorEsquerdo2, j);//VELOCIDADE
             delay(100);
           break;
@@ -171,7 +179,7 @@ void loop()
             digitalWrite(motorEsquerdo1, LOW);
             digitalWrite(motorDireito1, LOW);
             analogWrite(motorDireito2, i);//VELOCIDADE
-            j = (i/2)//METADE DA VELOCIDADE
+            j = (i/2);//METADE DA VELOCIDADE
             analogWrite(motorEsquerdo2, j);//VELOCIDADE
             delay(100);
           break;
@@ -194,4 +202,3 @@ void loop()
   }
  delay(100); 
 }
-
